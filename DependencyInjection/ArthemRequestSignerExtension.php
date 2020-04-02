1bc5516e2e3db14985c7291e047c08fbe5263b9f
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-04-02 16:35:03 +0200

    fix DI

--- DependencyInjection/ArthemRequestSignerExtension.php
+++ DependencyInjection/ArthemRequestSignerExtension.php
@@ -14,7 +14,7 @@ class ArthemRequestSignerExtension extends Extension
     /**
      * @var array
      */
-    private $signerAdapterFactories = [];
+    private $signerAdapterFactories;
 
     /**
      * {@inheritdoc}
